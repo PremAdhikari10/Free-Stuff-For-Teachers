4177c4958bf76ae59f74d45b0ea65188dcf94466
Charan Niroula <charanniroula02@gmail.com>
2024-03-26 13:08:26 -0500

    minor changes

--- src/Login_SignUP/SignIn.js
+++ src/Login_SignUP/SignIn.js
@@ -41,7 +41,7 @@ export default function SignIn() {
                   <img src="https://images.unsplash.com/photo-1603796846097-bee99e4a601f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
                       alt="signing" 
                       className="w-full rounded-2xl"
-                  />
+                  />   
               </div>
               <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
                   <form onSubmit={onSubmit}>
